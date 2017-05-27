a27c83bc4310721fbee0a6408632e3e3d8b7d269
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-05-27 01:37:26 -0700

    args parser

--- lab3a.c
+++ lab3a.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void print_usage(){
+    printf("Usage: lab3a file_system_name\n");
+}
 
 int main(int argc, char* argv[]){
-    printf("All working\n");
 
+//name of file system to be analyzed
+char* filename;
+
+    if(argc != 2){
+        fprintf(stderr, "Wrong number of arguments provided.\n");
+        print_usage();
+        exit(1);
+    } else{
+        filename = malloc(sizeof(char) * strlen(argv[1]+1));
+        if(filename == NULL){
+            fprintf(stderr, "Error allocating memory: %s\n",strerror(errno));
+            exit(1);
+        }
+    }
+
+//end of main
 }
