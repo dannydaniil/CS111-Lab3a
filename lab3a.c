bdbd9eeeb06d5f9a75d3f7bc296a38fb3333d7e8
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-05-27 02:12:25 -0700

    opens file

--- lab3a.c
+++ lab3a.c
@@ -3,26 +3,45 @@
 #include <string.h>
 #include <errno.h>
 
+//global variables
+char* fs_name;
+int fs_fd;
+
+void analyzeSuper(){
+    
+
+}
+
 void print_usage(){
     printf("Usage: lab3a file_system_name\n");
 }
 
 int main(int argc, char* argv[]){
 
-//name of file system to be analyzed
-char* filename;
-
     if(argc != 2){
         fprintf(stderr, "Wrong number of arguments provided.\n");
         print_usage();
         exit(1);
     } else{
-        filename = malloc(sizeof(char) * strlen(argv[1]+1));
-        if(filename == NULL){
+        fs_name = malloc(sizeof(char) * strlen(argv[1]+1));
+        if(fs_name == NULL){
             fprintf(stderr, "Error allocating memory: %s\n",strerror(errno));
-            exit(1);
+            exit(2);
         }
     }
+
+    fs_fd = open(fs_name, O_RDONLY);
+    if( fs_fd == -1 ){
+        fprintf(stderr, "Error while opening file%s\n",strerror(errno));
+        exit(2);
+    }
+
+
+
+
+
+analyzeSuper();
+
 
 //end of main
 }
