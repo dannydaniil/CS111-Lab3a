93ce1000e85a5bf0d86e8ca3a76e839124acd516
richardyu123 <richard95yu@gmail.com>
2017-05-28 02:42:12 -0700

    More loops

--- lab3a.c
+++ lab3a.c
@@ -14,9 +14,7 @@ char* fs_name;
 int fs_fd, directory_fd, indirect_fd;
 struct super_t * super;
 
-uint8_t buffer_8;
-uint16_t buffer_16;
-uint32_t buffer_32;
+int curr_entry, curr_offset;
 
 void print_error_message(int err_num, int exit_code) {
     fprintf(stderr, "%s\n", strerror(err_num));
@@ -28,48 +26,83 @@ void analyzeSuper(){
 
 }
 
-void generateDirectoryMessage(int curr_offset, int curr_entry) {
+void generateDirectoryMessage(int end_limit) {
     //TODO: replace with body of analyzeDirectory();
     uint8_t name_length;
     uint16_t entry_length;
     uint32_t inode;
-    if (pread(fs_fd, &name_length, 1, curr_offset + 6) == -1) { print_error_message(errno, 2); }
-    if (pread(fs_fd, &entry_length, 2, curr_offset + 4) == -1) { print_error_message(errno, 2); }
-    if (pread(fs_fd, &inode, 4, curr_offset) == -1) { print_error_message(errno, 2); }
+    while (curr_offset < end_limit) {
+        if (pread(fs_fd, &name_length, 1, curr_offset + 6) == -1) { print_error_message(errno, 2); }
+        if (pread(fs_fd, &entry_length, 2, curr_offset + 4) == -1) { print_error_message(errno, 2); }
+        if (pread(fs_fd, &inode, 4, curr_offset) == -1) { print_error_message(errno, 2); }
 
-    if (inode == 0) {
-        curr_offset += entry _length;
-        curr_entry;
-    } else {
-//TODO:replace inode_parent with whatever daniel uses 
-        char name_char;
-        dprintf(directory_fd, "%d,%d,%d,%d,%d,\"", inode_parent, curr_entry, entry_length, name_length, inode);
-        curr_entry++;
-        int l;
-        for (l = 0; l < name_length; l++) {
-            if (pread(fs_fd, &name_char, 1, curr_offset + 8 + l) == -1) { print_error_message(errno, 2); }
-            dprintf(directory_fd, "%c", name_char);
+        if (inode == 0) {
+            curr_offset += entry _length;
+            curr_entry;
+        } else {
+    //TODO:replace inode_parent with whatever daniel uses 
+            char name_char;
+            dprintf(directory_fd, "%d,%d,%d,%d,%d,\"", inode_parent, curr_entry, entry_length, name_length, inode);
+            curr_entry++;
+            int l;
+            for (l = 0; l < name_length; l++) {
+                if (pread(fs_fd, &name_char, 1, curr_offset + 8 + l) == -1) { print_error_message(errno, 2); }
+                dprintf(directory_fd, "%c", name_char);
+            }
+            dprintf(directory_fd, "\"\n");
+            curr_offset += entry_length;
         }
-        dprintf(directory_fd, "\"\n");
-        curr_offset += entry_length;
     }
 }
 
 void analyzeDirectory() {
     directory_fd = open("directory.csv", OWRONLY | O_CREAT | O_TRUNC, S_IRWXU);
     int i, j, k;
-    int curr_entry;
     //TODO: get directory_count
     for (i = 0; i < directory_count; i++) {
+        //direct blocks
         curr_entry = 0;
+        uint32_t offset;
         for (j = 0; j < BLOCK_COUNT; j++) {
-            uint32_t offset;
             //TODO" need node
             if (pread(fs_fd, offset, 4,... ) == -1) { print_error_message(errno, 2); }
             if (offset == 0) { continue; }
-            int curr_offset = super->blockSize * offset;
-            for (k = 0; k < super->blockSize; i++) {
-                generateDirectoryMessage(curr_offset, curr_entry); 
+            curr_offset = super->blockSize * offset;
+            generateDirectoryMessage(super->blockSize * dataOffset + super->blockSize); 
+        }
+        
+        //indirect blocks
+        //TODO: get pread arg
+        if (pread(fs_fd, &offset, 4, ..) == -1) { print_error_message(errno, 2); }
+        if (offset == 0) { continue; }
+        for (j = 0; j < super->blockSize / 4; j++) {
+            //depends on how inodes are organized
+            curr_offset = super->blockSize * dataOffset + (j * 4);
+            uint32_t block;
+            if (pread(fs_fd, &block, 4, curr_offset) == -1) { print_error_message(errno, 2); }
+            if (block != 0) {
+                curr_offset = block * super->blockSize;
+                generateDirectoryMessage(block * super->blockSize + super->blockSize);
+            }
+        }
+
+        //double indirect blocks
+        //TODO: find where the double indirect blocks are
+        if (pread(fs_fd, &offset, 4, ...) == -1) { print_error_message(errno, 2); }
+        if (offset == 0) { continue; }
+        for (j = 0; j < super->blockSize / 4; j ++) {
+            //find how inodes are organized
+            curr_offset = super->blockSize * dataOffset + (j * 4);
+            uint32_t block;
+            if (pread(fs_fd, &block, 4, curr_offset) == -1) { print_error_message(errno, 2); }
+            if (block == 0) { continue; }
+            int k;
+            for (k = 0; k < super->blockSize / 4; k++) {
+                uint32_t block2
+                if (pread(fs_fd, &block2, 4, block * super->blockSize + (k * 4)) == -1) { print_error_message(errno, 2); }
+                if (block == 0) { continue; }
+                curr_ofset = block2 * super->blockSize;
+                generateDirectoryMessage(block2 * super->blockSize + super->blockSize);
             }
         }
     }
