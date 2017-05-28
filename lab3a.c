90ac5960c69911366ee3a9283245f9488fa6bfc6
richardyu123 <richard95yu@gmail.com>
2017-05-27 17:12:43 -0700

    more minor changes

--- lab3a.c
+++ lab3a.c
@@ -7,13 +7,14 @@
 #include <string.h>
 #include <errno.h>
 
+#define BLOCK_COUNT 12
+
 //global variables
 char* fs_name;
-int fs_fd, directory_fd;
-static int BLOCK_COUNT = 12;
+int fs_fd, directory_fd, indirect_fd;
 
-void print_error_message(int errnum, int exit_code) {
-    fprintf(stderr, "%s\n", strerror(errno));
+void print_error_message(int err_num, int exit_code) {
+    fprintf(stderr, "%s\n", strerror(err_num));
     exit(exit_code);
 }
 
@@ -38,7 +39,14 @@ void analyzeDirectory() {
 }
 
 void analyzeIndirect() {
-
+    indirect_fd = creat("indirect.csv", S_IRWXU);
+    int i, j;
+    int curr_entry;
+    //TODO: get inode_count
+    for (i = 0; i < inode_count; i++) {
+        curr_entry = 0;
+        uint32_t curr_block;
+    }
 }
 
 void print_usage(){
