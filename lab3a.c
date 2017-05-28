42bd79ed3f338eb35f67f06c9f348c2426f5acd0
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-05-28 14:34:35 -0700

    include file

--- lab3a.c
+++ lab3a.c
@@ -3,13 +3,31 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "ext2_fs.h"
 
 //global variables
 char* fs_name;
 int fs_fd;
 
+
+void print_error_message(int err_num, int exit_code) {
+    fprintf(stderr, "%s\n", strerror(err_num));
+    exit(exit_code);
+}
+
+
+
 void analyzeSuper(){
-    //use pread to read in superblock struct
+    //pread(fd, buff, count , offset);
+
+    uint32_t block_num;
+    pread(fs_fd,&block_num, sizeof(uint32_t) ,1024 + sizeof(uint32_t));
+    print_error_message(errno,2);
+
+    
 
 
 
@@ -28,15 +46,13 @@ int main(int argc, char* argv[]){
     } else{
         fs_name = malloc(sizeof(char) * strlen(argv[1]+1));
         if(fs_name == NULL){
-            fprintf(stderr, "Error allocating memory: %s\n",strerror(errno));
-            exit(2);
+            print_error_message(errno,2);
         }
     }
 
     fs_fd = open(fs_name, O_RDONLY);
     if( fs_fd == -1 ){
-        fprintf(stderr, "Error while opening file%s\n",strerror(errno));
-        exit(2);
+        print_error_message(errno,2);
     }
 
 
